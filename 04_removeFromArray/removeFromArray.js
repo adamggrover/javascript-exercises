ad7754b55a76095831f9be338cccb662ee1450ea
Adam Grover <adamggrover@gmail.com>
2022-07-12 10:07:52 +0100

    Change splice method count number

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
 const removeFromArray = function(array, a) {
-    for (let x = 0; x < array.length; x++){
+    for (let x = 0; x < array.length -1; x++){
         if (a == array[x]){
-          array.splice(x,x);
-          return array;
+          array.splice(x,1);
+          
         }
     };
+
+    return array
 };
 
 // Do not edit below this line
