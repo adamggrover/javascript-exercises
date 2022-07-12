c0fdbb85b34c35a6174edc0c549b8627321e81f3
Adam Grover <adamggrover@gmail.com>
2022-07-12 09:56:28 +0100

    Write function to pass test one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, a) {
+    for (let x = 0; x < array.length; x++){
+        if (a == array[x]){
+          array.splice(x,x);
+          return array;
+        }
+    };
 };
 
 // Do not edit below this line
