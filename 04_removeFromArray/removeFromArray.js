4a49acc3feb135276c966cb025dff9b6fc5e2b3c
Adam Grover <adamggrover@gmail.com>
2022-07-15 17:14:21 +0100

    Change to use array.filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,5 @@
-const removeFromArray = function(array, a) {
-    for (let x = 0; x < array.length -1; x++){
-        if (a == array[x]){
-          array.splice(x,1);
-          
-        }
-    };
-
-    return array
+const removeFromArray = function(array,...args) {
+  return array.filter(x => !args.includes(i));
 };
 
 // Do not edit below this line
