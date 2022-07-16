1dceca34185c32767740e8eb8bfbf8df242d6cbe
Adam Grover <adamggrover@gmail.com>
2022-07-16 08:15:44 +0100

    Change structure of function
    Wanted to make it clearer how the code was working
    and make it more readable

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 const removeFromArray = function(array,...args) {
-  return array.filter(x => !args.includes(x));
-};
+  return array.filter(checkArray = (item) => {
+    return !args.includes(item)})
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
